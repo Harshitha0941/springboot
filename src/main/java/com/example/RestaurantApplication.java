b3f97374414b55565b9b2e264b6237f2f69edb12
Harshitha0941 <84893651+Harshitha0941@users.noreply.github.com>
2021-06-01 11:10:53 +0530

    Update RestaurantApplication.java

--- src/main/java/com/example/RestaurantApplication.java
+++ src/main/java/com/example/RestaurantApplication.java
@@ -3,12 +3,20 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class RestaurantApplication {
+public class RestaurantApplication extends SpringBootServletInitialize
+{
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context= SpringApplication.run(RestaurantApplication.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(RestaurantApplication.class);
+    }
+
 
 }
